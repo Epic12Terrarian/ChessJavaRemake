1e7b150426dab8bea04de177b84b1c5f0604f124
Epic12Terrarian <136501079+Epic12Terrarian@users.noreply.github.com>
2025-01-28 11:27:09 -0500

    Update Piece.java
    Merging TempPiece into the normal Piece file

--- chess/src/Piece.java
+++ chess/src/Piece.java
@@ -2,6 +2,7 @@ public class Piece {
     char piece;
     Integer[] location = {0,0};
     Integer tempInt = 946203842;
+    Integer tempInt2 = 907981;
     boolean tempBoolean = true;
     int team = 0; //0 for white, 1 for black (uppercase and lowercase is how I'll distinguish them apart)
 
@@ -9,13 +10,44 @@ public class Piece {
     public Piece(char piece, Integer[] location){
         this.piece = piece;
         this.location = location;
+        this.tempInt = location[0];
+        this.tempInt2 = location[1];
+        App.board[tempInt][tempInt2] = piece;
     }
     public boolean canMove(Integer[] location) {
         if(piece == 'R'){ //A method that helps figure out how a rook can move || will need to be revised later to take into account pieces potentially being in the path
             if (location[0] == this.location[0] || location[1] == this.location[1]) {
-                return true;
+                tempBoolean = true;
             }
         }
+        if(piece == 'N'){
+            if(location[0] == this.location[0] + 2 && location[1] == this.location[1] + 1) {
+            	tempBoolean = true;
+            }
+            if(location[0] == this.location[0] + 2 && location[0] == this.location[1] - 1) {
+            	tempBoolean = true;
+            }
+            if(location[0] == this.location[0] - 2 && location[0] == this.location[1] - 1) {
+            	tempBoolean = true;
+            }
+            if(location[0] == this.location[0] - 2 && location[0] == this.location[1] + 1) {
+            	tempBoolean = true;
+            }
+            if(location[0] == this.location[0] + 1 && location[0] == this.location[1] + 2) {
+            	tempBoolean = true;
+            }
+            if(location[0] == this.location[0] + 1 && location[0] == this.location[1] - 2) {
+                tempBoolean = true;
+            }
+            if(location[0] == this.location[0] - 1 && location[0] == this.location[1] - 2) {
+                tempBoolean = true;
+            }
+            if(location[0] == this.location[0] - 1 && location[0] == this.location[1] + 2) {
+                tempBoolean = true;
+            }           
+        }
+
+        
         tempInt = location[0];
         if (tempInt < 1 || tempInt > 8) {
             tempBoolean = false;
@@ -31,7 +63,13 @@ public class Piece {
         this.piece = piece;
     }
     public void setLocation(Integer[] location) {
+        this.tempInt = this.location[0]
+        this.tempInt = this.location[1]
+        App.board[tempInt][tempInt2] = " ";
         this.location = location;
+        this.tempInt = location[0];
+        this.tempInt2 = location[1]
+        App.board[tempInt][tempInt2] = piece;
     }
     public Integer[] getLocation() {
         return location;
