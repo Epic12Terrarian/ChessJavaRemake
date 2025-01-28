e592a24205ceed4a9620dbb59397cc4536a9ae8c
Epic12Terrarian <136501079+Epic12Terrarian@users.noreply.github.com>
2025-01-28 08:16:08 -0500

    Rook / Board work
    Continued work on the Rook part of the Piece class, and started work on making the board so I can test and make sure pieces are working as intended.

--- chess/src/App.java
+++ chess/src/App.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class App {
+    public static String[][] board = new String[10][10];
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                board[i][j] = " ";
+            }
+        }
+
+        System.out.println("|" + board[3][2] + "|");
+
+    }
+    public static void buildBoard() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
     }
 }
--- chess/src/Piece.java
+++ chess/src/Piece.java
@@ -1,23 +1,36 @@
 public class Piece {
     char piece;
-    Integer[][] location = {{0},{0}};
+    Integer[] location = {0,0};
+    Integer tempInt = 946203842;
+    boolean tempBoolean = true;
+    int team = 0; //0 for white, 1 for black (uppercase and lowercase is how I'll distinguish them apart)
 
     
-    
-    public Piece(char piece, Integer[][] location){
+    public Piece(char piece, Integer[] location){
         this.piece = piece;
         this.location = location;
     }
-    public boolean canMove(Integer[][] location) {
-        if(piece == 'R'){
-            
+    public boolean canMove(Integer[] location) {
+        if(piece == 'R'){ //A method that helps figure out how a rook can move || will need to be revised later to take into account pieces potentially being in the path
+            if (location[0] == this.location[0] || location[1] == this.location[1]) {
+                return true;
+            }
         }
-        return false;
+        tempInt = location[0];
+        if (tempInt < 1 || tempInt > 8) {
+            tempBoolean = false;
+        } //above and below determine the board size being a standard 8 by 8
+        tempInt = location[1];
+        if (tempInt < 1 || tempInt > 8) {
+            tempBoolean = false;
+        }
+
+        return tempBoolean;
     }
     public void setPiece(char piece) {
         this.piece = piece;
     }
-    public void setLocation(Integer[][] location) {
+    public void setLocation(Integer[] location) {
         this.location = location;
     }
     public Integer[][] getLocation() {
