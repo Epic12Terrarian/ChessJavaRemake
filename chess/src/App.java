f7a17f94f9be875aa0ffee39ffcc328d64d31eb1
Epic12Terrarian <136501079+Epic12Terrarian@users.noreply.github.com>
2025-01-28 09:12:51 -0500

    Update App.java
    minor changes to the app.java class

--- chess/src/App.java
+++ chess/src/App.java
@@ -4,13 +4,14 @@ public class App {
     public static String[][] board = new String[10][10];
     
     public static void main(String[] args) throws Exception {
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 10; i++){ //this loop is used to fill the board file so I can display it later and not need to worry about anything being undefined
             for(int j = 0; j < 10; j++){
                 board[i][j] = " ";
             }
         }
 
-        System.out.println("|" + board[3][2] + "|");
+        System.out.println("Text in console to make sure this file is still functional");
+        
 
     }
     public static void buildBoard() {
